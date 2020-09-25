32d2614a446707ee5c4b1819aa114e3f782fa315
FyreByrns <horace5413@gmail.com>
2020-09-25 02:49:02 -0700

    Update Vector2.cs

--- Vector2.cs
+++ Vector2.cs
@@ -4,10 +4,15 @@ class Vector2 {
     public float Length2 => x * x + y * y;
     public float Length => (float)System.Math.Sqrt(Length2);
 
-    public Vector2 Normal() {
+    public Vector2 RawNormal =>
+        new Vector2(y, -x);
+    public Vector2 Normal =>
+        RawNormal.Normalized();
+
+    public Vector2 Normalized() {
         Vector2 result = new Vector2();
 
-        if (Length > 0.0f) {
+        if (Length > 0) {
             result.x = x / Length;
             result.y = y / Length;
         }
@@ -15,11 +20,22 @@ class Vector2 {
         return result;
     }
 
+    public float DotProduct(Vector2 other) =>
+        DotProduct(this, other);
+
+    public Vector2 Project(Vector2 onto) =>
+        Project(this, onto);
+
     public static float DotProduct(Vector2 va, Vector2 vb) =>
         va.x * vb.x + va.y * vb.y;
 
+    public static Vector2 Project(Vector2 v, Vector2 onto) {
+        float dp = DotProduct(v, onto);
+        return onto * dp;
+    }
+
     public static bool operator ==(Vector2 va, Vector2 vb) =>
-        va != null && vb != null &&
+        (object)va != null && (object)vb != null &&
         va.x == vb.x &&
         va.y == vb.y;
 
@@ -27,9 +43,11 @@ class Vector2 {
         !(va == vb);
 
     public static Vector2 operator +(Vector2 va, Vector2 vb) =>
-        new Vector2(va.x + vb.x, va.y + vb.y);
+        new Vector2(va?.x + vb?.x, 
+                    va?.y + vb?.y);
     public static Vector2 operator -(Vector2 va, Vector2 vb) =>
-        new Vector2(va.x - vb.x, va.y - vb.y);
+        new Vector2(va?.x - vb?.x, 
+                    va?.y - vb?.y);
 
     public static Vector2 operator *(Vector2 va, Vector2 vb) =>
         new Vector2(va.x * vb.x, va.y * vb.y);
@@ -37,7 +55,7 @@ class Vector2 {
         new Vector2(va.x / vb.x, va.y / vb.y);
 
     public static Vector2 operator *(Vector2 v, float f) =>
-        new Vector2(v.x * f, v.y * f);
+        new Vector2(v?.x * f, v?.y * f);
     public static Vector2 operator /(Vector2 v, float f) =>
         new Vector2(v.x / f, v.y / f);
 
@@ -50,10 +68,19 @@ class Vector2 {
         this.x = x;
         this.y = y;
     }
+    public Vector2(float?x, float? y) {
+        this.x = x ?? 0;
+        this.y = y ?? 0;
+    }
 
     public override int GetHashCode() =>
         base.GetHashCode();
 
     public override bool Equals(object obj) =>
         obj is Vector2 v && v == this;
+}
+
+static class Vector2Extensions {
+    public static Point ToPoint(this Vector2 me) =>
+        new Point((int)me.x, (int)me.y);
 }
