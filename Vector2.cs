f19e954d1739f70cc86163446ac40a5376e64a46
FyreByrns <horace5413@gmail.com>
2020-10-26 12:35:02 -0700

    Make Vector2 generic

--- Vector2.cs
+++ Vector2.cs
@@ -1,86 +1,84 @@
-class Vector2 {
-    public float x, y;
+public class Vector2<T> where T : struct {
+	public T x, y;
 
-    public float Length2 => x * x + y * y;
-    public float Length => (float)System.Math.Sqrt(Length2);
+	public T Length2 => (dynamic)x * x + (dynamic)y * y;
+	public T Length => (dynamic)System.Math.Sqrt((dynamic)Length2);
 
-    public Vector2 RawNormal =>
-        new Vector2(y, -x);
-    public Vector2 Normal =>
-        RawNormal.Normalized();
+	public Vector2<T> RawNormal =>
+		new Vector2<T>((dynamic)y, -(dynamic)x);
+	public Vector2<T> Normal =>
+		RawNormal.Normalized();
 
-    public Vector2 Normalized() {
-        Vector2 result = new Vector2();
+	public Vector2<T> Normalized() {
+			Vector2<T> result = new Vector2<T>();
 
-        if (Length > 0) {
-            result.x = x / Length;
-            result.y = y / Length;
-        }
+			if ((dynamic)Length > 0) {
+				result.x = (dynamic)x / Length;
+				result.y = (dynamic)y / Length;
+			}
 
-        return result;
-    }
+			return result;
+		}
 
-    public float DotProduct(Vector2 other) =>
-        DotProduct(this, other);
+	public float DotProduct(Vector2<T> other) =>
+			DotProduct(this, other);
 
-    public Vector2 Project(Vector2 onto) =>
-        Project(this, onto);
+	public Vector2<T> Project(Vector2<T> onto) =>
+			Project(this, onto);
 
-    public static float DotProduct(Vector2 va, Vector2 vb) =>
-        va.x * vb.x + va.y * vb.y;
+	public static float DotProduct(Vector2<T> va, Vector2<T> vb) =>
+			(dynamic)va.x * vb.x + (dynamic)va.y * vb.y;
 
-    public static Vector2 Project(Vector2 v, Vector2 onto) {
-        float dp = DotProduct(v, onto);
-        return onto * dp;
-    }
+	public static Vector2<T> Project(Vector2<T> v, Vector2<T> onto) {
+			float dp = DotProduct(v, onto);
+			return (dynamic)onto * dp;
+		}
 
-    public static bool operator ==(Vector2 va, Vector2 vb) =>
-        va is object && vb is object &&
-        va.x == vb.x &&
-        va.y == vb.y;
+	public static bool operator ==(Vector2<T> va, Vector2<T> vb) =>
+			va is object && vb is object &&
+			(dynamic)va.x == vb.x &&
+			(dynamic)va.y == vb.y;
 
-    public static bool operator !=(Vector2 va, Vector2 vb) =>
-        !(va == vb);
+	public static bool operator !=(Vector2<T> va, Vector2<T> vb) =>
+			!(va == vb);
 
-    public static Vector2 operator +(Vector2 va, Vector2 vb) =>
-        new Vector2(va?.x + vb?.x, 
-                    va?.y + vb?.y);
-    public static Vector2 operator -(Vector2 va, Vector2 vb) =>
-        new Vector2(va?.x - vb?.x, 
-                    va?.y - vb?.y);
+	public static Vector2<T> operator +(Vector2<T> va, Vector2<T> vb) =>
+			new Vector2<T>((dynamic)va.x + vb.x,
+						   (dynamic)va.y + vb.y);
+	public static Vector2<T> operator -(Vector2<T> va, Vector2<T> vb) =>
+			new Vector2<T>((dynamic)va.x - vb.x,
+						   (dynamic)va.y - vb.y);
 
-    public static Vector2 operator *(Vector2 va, Vector2 vb) =>
-        new Vector2(va.x * vb.x, va.y * vb.y);
-    public static Vector2 operator /(Vector2 va, Vector2 vb) =>
-        new Vector2(va.x / vb.x, va.y / vb.y);
+	public static Vector2<T> operator *(Vector2<T> va, Vector2<T> vb) =>
+		new Vector2<T>((dynamic)va.x * vb.x, (dynamic)va.y * vb.y);
+	public static Vector2<T> operator /(Vector2<T> va, Vector2<T> vb) =>
+		new Vector2<T>((dynamic)va.x / vb.x, (dynamic)va.y / vb.y);
 
-    public static Vector2 operator *(Vector2 v, float f) =>
-        new Vector2(v?.x * f, v?.y * f);
-    public static Vector2 operator /(Vector2 v, float f) =>
-        new Vector2(v.x / f, v.y / f);
+	public static Vector2<T> operator *(Vector2<T> v, float f) =>
+		new Vector2<T>((dynamic)v.x * f, (dynamic)v.y * f);
+	public static Vector2<T> operator /(Vector2<T> v, float f) =>
+		new Vector2<T>((dynamic)v.x / f, (dynamic)v.y / f);
 
-    public Vector2() { }
-    public Vector2(Vector2 v) {
-        x = v.x;
-        y = v.y;
-    }
-    public Vector2(float x, float y) {
-        this.x = x;
-        this.y = y;
-    }
-    public Vector2(float?x, float? y) {
-        this.x = x ?? 0;
-        this.y = y ?? 0;
-    }
+	public Vector2<TCast> Cast<TCast>() where TCast : struct =>
+		new Vector2<TCast>((TCast)(dynamic)x, (TCast)(dynamic)y);
 
-    public override int GetHashCode() =>
-        base.GetHashCode();
+	public Vector2() { }
+	public Vector2(Vector2<T> v) {
+			x = v.x;
+			y = v.y;
+		}
+	public Vector2(T x, T y) {
+			this.x = x;
+			this.y = y;
+		}
+	public Vector2(T? x, T? y) {
+			this.x = x ?? default;
+			this.y = y ?? default;
+		}
 
-    public override bool Equals(object obj) =>
-        obj is Vector2 v && v == this;
-}
+	public override int GetHashCode() =>
+			base.GetHashCode();
 
-static class Vector2Extensions {
-    public static Point ToPoint(this Vector2 me) =>
-        new Point((int)me.x, (int)me.y);
+	public override bool Equals(object obj) =>
+			obj is Vector2<T> v && v == this;
 }
